6259719eaeffade7a6e71bac1791b816bdad6cd9
kaduce2009@hotmail.com <kaduce2009@hotmail.com>
2020-06-02 20:35:19 -0300

    Primeiro Commit

---
+++ scripts/create-point.js
@@ -0,0 +1,40 @@
+
+function populateUFs(){
+    const uFSelect = document.querySelector("select[name=uf]")
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then((res)=>{return res.json()})
+    .then(states => {
+
+        for(const state of states){
+            uFSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
+
+    })
+}
+
+populateUFs()
+
+function getCities(event){
+    const citySelect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
+    const ufValue = event.target.value
+
+    const indexOFSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOFSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    fetch(url)
+    .then((res)=>{return res.json()})
+    .then(cities => {
+
+        for(const city of cities){
+            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        }
+        citySelect.disabled =false
+    })
+}
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change", getCities)
